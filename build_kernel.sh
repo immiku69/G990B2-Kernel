9d09bd63227778c9fb48739a1b8763a390a60b03
Aniket <youknowmexd69@gmail.com>
2025-09-28 09:26:34 +0530

    Try 69

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,13 +4,12 @@ export ARCH=arm64
 export SUBARCH=arm64
 mkdir -p out
 
-BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-CROSS_COMPILE=$(pwd)/toolchain/aarch64-linux-android-4.9-toolchain/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
-CLANG_TRIPLE=aarch64-linux-gnu-
+export PATH=$(pwd)/toolchain/neutron-clang/bin:$PATH
+export CC=clang
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE
 make -j64 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
